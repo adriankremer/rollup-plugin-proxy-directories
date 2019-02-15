9c9956913792ac204b8255c626c55775ab6de71e
Adrian Kremer <adrian.kremer@namics.com>
2019-02-15 15:54:11 +0100

    Added feature to generate a .gitignore file for the proxy directories

--- src/index.js
+++ src/index.js
@@ -1,7 +1,29 @@
-const { writeFileSync, mkdirSync } = require('fs');
+const {
+  writeFileSync,
+  mkdirSync,
+  readFileSync,
+  createWriteStream,
+  existsSync,
+} = require('fs');
+
+const populateIgnoreFile = (fileName, modules) => {
+  const ignoreFile = existsSync(fileName) && readFileSync(fileName, "utf-8");
+  const stream = createWriteStream(fileName, { flags: 'a' });
+  if (!ignoreFile) {
+    process.stdout.write(`💎 \x1b[35mCreating '${fileName}' with proxy directories.\x1b[0m\r\n`)
+  } else {
+    process.stdout.write(`💈 \x1b[35mUpdating '${fileName}' with proxy directories.\x1b[0m\r\n`);
+  }
+  modules.forEach(([module]) => {
+    if (!ignoreFile || ignoreFile.indexOf(module) < 0) {
+      stream.write(`${module}\r\n`);
+    }
+  });
+  stream.end();
+};
 
 export default function proxyDirectories(options = {}) {
-  const { packageName, files } = options;
+  const { packageName, files, ignoreFile } = options;
   let proxiesGenerated = false;
   return {
     name: 'proxy-directories',
@@ -22,16 +44,20 @@ export default function proxyDirectories(options = {}) {
           "types": "../ts"
         }`;
 
-        Object.entries(files)
-          .filter(([module]) => module !== 'index')
-          .forEach(([module]) => {
-            mkdirSync(module);
-            writeFileSync(`${module}/package.json`, createProxyPackage(module));
-          });
+        const modules = Object.entries(files)
+          .filter(([module]) => module !== 'index');
+
+        modules.forEach(([module]) => {
+          mkdirSync(module);
+          writeFileSync(`${module}/package.json`, createProxyPackage(module));
+        });
 
         ['lib', 'es'].forEach(dir => {
           writeFileSync(`${dir}/package.json`, createDirPackage(dir));
         });
+
+        ignoreFile && populateIgnoreFile(".gitignore", modules);
+
         proxiesGenerated = true;
       }
     }
