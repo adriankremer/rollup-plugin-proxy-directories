255af4deee670581ec8903e1e6922dbfa80ddae2
Adrian Kremer <adrian.kremer@namics.com>
2019-02-20 09:23:28 +0100

    Added deep proxy traversal, added buildDir resolving by package, added publicfile gathering

--- README.md
+++ README.md
@@ -1,10 +1,39 @@
 # rollup-plugin-proxy-directories
 
-creates proxy directories for libraries to make direct imports easier.
+creates proxy directories for libraries without configuration to make direct imports easier.
 
 ## Example
+**Source**
+```
+├── Button
+│   ├── Button.tsx
+│   ├── ButtonHook.tsx
+│   └── index.ts
+├── index.ts
+```
+
+**Build**
+```
+├── Button
+│   ├── ButtonHooks
+│   │   └── package.json
+│   └── package.json
+├── es
+│   ├── Button
+│   │   ├── ButtonHooks.js
+│   │   └── index.js
+│   ├── index.js
+├── lib
+│   ├── Button
+│   │   ├── ButtonHooks.js
+│   │   └── index.js
+│   ├── index.js
+```
+
+**Usage**
 ```js
 import Button from 'ui-library/button';
+import ButtonHook from 'ui-library/button/buttonHook';
 ```
 inspired by this article: [how-to-create-javascript-libraries-in-2018](https://developers.livechatinc.com/blog/how-to-create-javascript-libraries-in-2018-part-2/)
 
@@ -24,38 +53,13 @@ import pkg from "./package.json";
 export default {
   // ...
   plugins: [
-    proxyDirectories({
-      packageName: pkg.name,
-      files: {
-        button: "src/Button/index.ts"
-      },
-      gitIgnore: true,
-    })
+    proxyDirectories()
   ]
 };
 ```
 
-## Options
+## todos (please contribute)
 
-```js
-{
-  // the name of the library
-  // this will result in the proxy packages e.g. packageName/module
-  packageName: "name-of-the-library",
-
-  // files object which represent modules you want to proxy
-  files: {
-    moduleName: "path/to/module",
-  }
-
-  // generates or poulates a .gitignore file for the generated proxy-directories
-  ignoreFile: true,
-}
-```
-
-## todos
-
-* add console messages like "generating proxy-directories"
 * write tests :)
 
 
--- src/index.js
+++ src/index.js
@@ -1,58 +1,162 @@
-const { writeFileSync, mkdirSync, readFileSync, createWriteStream, existsSync } = require('fs');
+const {
+  writeFileSync,
+  readFileSync,
+  createWriteStream,
+  existsSync,
+  ensureDirSync,
+  readdirSync,
+  lstatSync } = require('fs-extra');
+const { join, dirname } = require('path');
+const rimraf = require('rimraf');
+const cwd = process.cwd();
+const chalk = require('chalk');
 
-const populateIgnoreFile = (fileName, modules) => {
+
+function message(message, cssColor, bold) {
+  process.stdout.write(
+    bold ? chalk.bold.keyword(cssColor)(message + '\r\n') : chalk.keyword(cssColor)(message + '\r\n')
+  );
+}
+
+function populateIgnoreFile(rootPath, fileName) {
+  const buildFolders = getBuildFolders(rootPath);
   const ignoreFile = existsSync(fileName) && readFileSync(fileName, 'utf-8');
   const stream = createWriteStream(fileName, { flags: 'a' });
-  if (!ignoreFile) {
-    process.stdout.write(`💎 \x1b[35mCreating '${fileName}' with proxy directories.\x1b[0m\r\n`);
-  } else {
-    process.stdout.write(`💈 \x1b[35mUpdating '${fileName}' with proxy directories.\x1b[0m\r\n`);
-  }
-  modules.forEach(([module]) => {
-    if (!ignoreFile || ignoreFile.indexOf(module) < 0) {
-      stream.write(`/${module}/\r\n`);
-    }
-  });
+  message(`\r\n💾  ${ignoreFile ? 'Updating' : 'Creating'} '${fileName}' with proxy directories.\r\n`, 'salmon');
+  buildFolders
+    .filter(name => !/\//.test(name))
+    .forEach((folder) => {
+      if (!ignoreFile || ignoreFile.indexOf(folder) < 0) {
+        stream.write(`/${folder}/\r\n`);
+      }
+    });
   stream.end();
-};
+}
 
-export default function proxyDirectories(options = {}) {
-  const { packageName, files, gitIgnore } = options;
-  let proxiesGenerated = false;
+function resolveDir(dir) {
+  if (!/\.(t|j)s$/.test(dir)) {
+    return dir;
+  }
+  return dirname(dir);
+}
+
+function getPackage(rootPath) {
+  return require(join(rootPath, "package.json"));
+}
+
+function getBuildDir(rootPath, type) {
+  const pkg = getPackage(rootPath);
+  try {
+    return resolveDir(pkg[type]);
+  } catch (e) {
+    return false;
+  }
+}
+
+function removeExt(path) {
+  return path.replace(/\.[^.]+$/, "");
+}
+
+function isPublicModule(path) {
+  return !/^_/.test(path);
+}
+
+function isDirectory(path) {
+  return lstatSync(path).isDirectory();
+}
+
+function isDirectoryName(path, fileName) {
+  const splittedPath = path.split("/");
+  const folderName = splittedPath[splittedPath.length - 2];
+  return folderName === removeExt(fileName);
+}
+
+function getSourcePath(rootPath) {
+  return join(rootPath, "src");
+}
+
+function getProxyPackageContents(rootPath, moduleName) {
+  const { name } = getPackage(rootPath);
+  const mainDir = getBuildDir(rootPath, 'main');
+  const moduleDir = getBuildDir(rootPath, 'module');
+  const typesDir = getBuildDir(rootPath, 'types') || getBuildDir(rootPath, 'typings');
+  const prefix = "../".repeat(moduleName.split("/").length);
+  const packageJson = Object.assign(
+    {
+      name: `${name}/${moduleName}`,
+      private: true,
+    },
+    mainDir ? { main: join(prefix, mainDir, moduleName) } : {},
+    moduleDir ? { module: join(prefix, moduleDir, moduleName) } : {},
+    typesDir ? { types: join(prefix, typesDir, moduleName) } : {}
+  );
+  return JSON.stringify(packageJson, null, 2);
+}
+
+function getPublicFiles(rootPath, prefix = "") {
+  return readdirSync(rootPath)
+    .filter(isPublicModule)
+    .reduce((acc, filename) => {
+      const path = join(rootPath, filename);
+      const childFiles = isDirectory(path) && getPublicFiles(path, filename);
+      return Object.assign(
+        childFiles || !isDirectoryName(path, filename) && { [ removeExt(join(prefix, filename))]: path },
+        acc,
+      );
+    }, {});
+}
+
+function getProxyFolders(rootPath) {
+  const publicFiles = getPublicFiles(getSourcePath(rootPath));
+  return Object.keys(publicFiles)
+    .map(name => name.replace(/\/index$/, ""))
+    .filter(name => name !== "index");
+}
+
+function getBuildFolders(rootPath) {
+  return [
+    getBuildDir(rootPath, 'main'),
+    getBuildDir(rootPath, 'unpkg'),
+    getBuildDir(rootPath, 'module'),
+    ...getProxyFolders(rootPath),
+  ].filter(Boolean);
+}
+
+function cleanBuild(rootPath) {
+  message(`\r\n🚽  Cleaning ${rootPath}`, 'salmon');
+  return getBuildFolders(rootPath)
+    .filter(name => !/\//.test(name))
+    .forEach(name => {
+      try {
+        rimraf.sync(name);
+        message(`- cleaned ${name}`, 'gray');
+      } catch (e) {
+        message(`Couldn't clean ${name}`, 'red');
+      }
+    });
+}
+
+function makeProxies(rootPath) {
+  message(`\r\n🎁  Making proxies in ${rootPath}`, 'salmon');
+  return getProxyFolders(rootPath).forEach(name => {
+    ensureDirSync(name);
+    writeFileSync(
+      `${name}/package.json`,
+      getProxyPackageContents(rootPath, name)
+    );
+    message(`- created ${name}`, 'gray');
+  });
+}
+
+export default function proxyDirectories() {
   return {
     name: 'proxy-directories',
 
-    writeBundle() {
-      if (!proxiesGenerated) {
-        const createProxyPackage = module => `{
-          "name": "${packageName}/${module}",
-          "private": true,
-          "main": "../lib/${module}",
-          "module": "../es/${module}",
-          "types": "../ts/${module}"
-        }`;
-
-        const createDirPackage = dir => `{
-          "name": "${packageName}/${dir}",
-          "private": true,
-          "types": "../ts"
-        }`;
-
-        const modules = Object.entries(files).filter(([module]) => module !== 'index');
-
-        modules.forEach(([module]) => {
-          mkdirSync(module);
-          writeFileSync(`${module}/package.json`, createProxyPackage(module));
-        });
-
-        ['lib', 'es'].forEach(dir => {
-          writeFileSync(`${dir}/package.json`, createDirPackage(dir));
-        });
-
-        gitIgnore && populateIgnoreFile('.gitignore', modules);
-
-        proxiesGenerated = true;
-      }
+    buildStart(options) {
+      options.input = getPublicFiles(getSourcePath(cwd));
+      cleanBuild(cwd);
+      makeProxies(cwd);
+      populateIgnoreFile(cwd, '.gitignore');
     }
   };
 }
